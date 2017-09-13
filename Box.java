07f2c8e066368066a41bd30b1316072468a8b5a6
Jenny Fish <jfish2018@pingry.org>
2017-09-13 16:01:53 -0400

    updated README and completed TODOs in Box

--- Box.java
+++ Box.java
@@ -3,14 +3,18 @@ public class Box<T> {
 
   private T contents;
   private boolean isFull;
-
-  // Make a new empty box
+  
+  /**
+   * Make a new empty box
+   */
   public Box(){
 	  super();
 	  this.isFull = false;
   }
-
-  // Make a new box pre-filled with contents
+  /**
+   * Make a new box pre-filled with contents
+   * @param t Contents of new box
+   */
   public Box(T t){
 	  super();
 	  contents = t;
@@ -43,38 +47,72 @@ public class Box<T> {
 	  contents = null;
 	  return save;
   }
+  
+  /**
+   * Fills the box
+   * @param contents Contents to fill the box with
+   * @return if the box was filled successfully
+   */
+  public boolean fill(T contents){
+	  if(!isFull){
+		  this.contents = contents;
+		  isFull = true;
+		  return true;
+	  }
+	  return false;
+  }
 
+  /**
+   * Overrides the toString method in Object
+   * @return String of contents, else null
+   */
   public String toString(){
 	  if(isFull)
 		return contents.toString();
-	  return "empty";
+	  return null;
+  }
+  
+  /**
+   * Prints the contents of the box
+   */
+  public void peek(){
+	  System.out.print("        ");
+	  System.out.println(contents);
+	  System.out.println("   ___________\n  /          /|\n /          / |\n/__________/  |\n|          |  / \n|          | / \n|__________|/ \n");
+	  
   }
 
+  /**
+   * Main method
+   * @param args Command line arguments
+   */
   public static void main(String[] args){
-
+	
     // Make two boxes
     Box<String> stringBox = new Box<String>();
     //TODO start the second box with contents inside
-    Box<Integer> intBox   = new Box<Integer>();
+    Box<Integer> intBox   = new Box<Integer>(5);
 
     // View contents (via toString method)
     System.out.println("The boxes contain: " + stringBox + ", " + intBox);
 
     //TODO Add something to a box
-
+	stringBox.fill("AAAAA");
 
 
     // View contents (via toString method)
     System.out.println("The boxes contain: " + stringBox + ", " + intBox);
 
     //TODO Test your peek method
-
+	stringBox.peek();
+	intBox.peek();
 
     // View contents (via toString method)
     System.out.println("The boxes contain: " + stringBox + ", " + intBox);
 
-    //TODO Empty a boxes
-
+    //TODO Empty the boxes
+	stringBox.empty();
+	intBox.empty();
 
 
     // View contents (via toString method)
--- README.md
+++ README.md
@@ -3,14 +3,34 @@ Box Data Structure
 
 A simple data structure primarily for instructional purposes.
 
+Overview
+--------
+   ___________
+  /          /|
+ /          / |
+/__________/  |
+|          |  /
+|          | /
+|__________|/
+
+
 Usage
 -----
+To run this program, you will need to click on the clone or download button, then copy the URL address. After this step, you must visit your terminal and paste the URL to initialize your git repository. Once you have downloaded it to your computer, you are ready to run the program.
 
 Authors
 -------
+Jenny Fish
 
 License
 -------
+Copyright (c) 2017 Jenny Fish
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 Tests
 -----
@@ -23,3 +43,7 @@ Todo List
 4. Complete preceding sections of this README
 5. Remove this todo list
 6. (optional) Make an even more interesting main program
+
+
+
+
